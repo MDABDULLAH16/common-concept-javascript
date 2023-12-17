6ff51ee3d36653b8a37c29a9ed6b610fdf144b1b
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-17 19:23:08 +0600

    trety and false cheek

---
+++ true-false.js
@@ -0,0 +1,29 @@
+//cheek truthy falsy value is js
+
+/*
+// true
+1. '0' is true
+2. " "
+
+// false
+1. 0 is false like boolian type
+2. "" is false
+*/
+// common method to cheek booth
+let x = "";
+if (x) {
+  //   console.log("value is truty");
+} else {
+  //   console.log("value is false");
+}
+
+// optional to cheek true false value
+// to cheek false
+let y = true;
+if (!y) {
+  console.log(y, "is false");
+}
+//to cheek true
+if (!!y) {
+  console.log(y, "is truee");
+}
