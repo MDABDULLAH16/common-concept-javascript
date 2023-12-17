cdf051df902f3623605f89bedc1255b4f7c60eaf
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-17 17:20:13 +0600

    primitive and non-primitiv

--- types.js
+++ types.js
@@ -1,3 +1,4 @@
+//javaScript is a dynamic tyeped Programming language
 let x = 5;
 let y = x;
 console.log(x, y);
@@ -7,6 +8,14 @@ console.log(x, y);
 let numbers = [23, 43, 53, 64, 65];
 // console.log(numbers);
 let number = numbers;
-console.log("after change", number);
-number = [11, 22, 33];
-console.log("after push", number);
+// console.log("after change", number);
+// number = [11, 22, 33];
+// console.log("after push", number);
+
+///non-primitive data types array,objects etc.
+
+let p = { job: "web developer" };
+let q = p;
+// q.job = " fronted developer";
+q = { job: "bancker" };
+console.log(p, q);
