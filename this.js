133de27c5e599b0474c9eb52e813ebc74af6ef80
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-24 23:39:27 +0600

    use strict mode

--- this.js
+++ this.js
@@ -1,9 +1,12 @@
-// function helloThis() {
-//   console.log(this);
-// }
+function helloThis() {
+  //   "use strict"; ai mode a kaj korbe na (this.rohim variable)
+  this.rohim = "rohim mia";
+  console.log(rohim);
+}
 
-// var a = 10;
-// helloThis();
+helloThis();
+
+var a = 10;
 
 let name = "abdullah";
 function myName() {
@@ -13,7 +16,20 @@ function myName() {
 }
 function second() {
   let welcome2 = "Hi ";
-
+  third();
   console.log(welcome2 + name);
 }
+function third() {
+  let welcome3 = "Hey! ";
+  console.log(welcome3 + name);
+}
 myName();
+
+function argu(a, b, c) {
+  const arg = arguments;
+  console.log(arg);
+  const sum = a + b + c;
+  return sum;
+}
+const total = argu(2, 3, 4, 6, 7);
+console.log(total);
